d8ce6158c2756cfe7ea9d6cd74734b2a2b0f020a
Roberto Santos <contato@robertosantos.net>
2016-08-09 16:32:52 +0100

    first commit.

---
+++ R/df2dist.R
@@ -0,0 +1,13 @@
+#' A data frame to distance matrix function
+#'
+#' This function allows you to convert from a data frame
+#' to a matrix of distance.
+#' @param dat Data frame to be converted to distance matrix.
+#' @keywords distance dataframe
+#' @export
+#' @examples
+#' df2dist()
+
+df2dist <- function(dat) {
+  return (1)
+}
